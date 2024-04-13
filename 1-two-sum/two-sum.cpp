9241aaf168d90153262fa87c59cf7b0cdbd62ed5
obbteam <73595348+obbteam@users.noreply.github.com>
2024-04-13 23:32:23 +0200

    Time: 260 ms (5.04%) | Memory: 12.7 MB (73.89%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,24 +3,13 @@
 class Solution {
     public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // init vars
         vector<int> twoIndices;
-        std::unordered_map<int, int> HashTable;
-        int capacity = nums.size();
-
-        // allocate nums into hash table
-        for (int i = 0; i < capacity; i++) {
-            HashTable.insert({nums[i], i});
-        }
-        //looping through all numbers untill sum != target
-        for (int i = 0; i < capacity; i++) {
-            int second = target - nums[i];
-            auto it = HashTable.find(second);
-            // ensure that second number exists in our HT and is not the same as nums[i]
-            if (it != HashTable.end() && it->second != i) {
-                twoIndices.push_back(i);
-                twoIndices.push_back(it->second);
-                break;
+        for(int i = 0; i < nums.size(); i++) {
+            for(int k = i+1; k < nums.size(); k++) {
+            if(nums[i]+nums[k]==target) {
+                    twoIndices.push_back(i);
+                    twoIndices.push_back(k);
+                }
             }
         }
         return twoIndices;
