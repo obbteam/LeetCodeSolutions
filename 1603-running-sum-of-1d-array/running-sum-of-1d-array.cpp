6c377260ee4150501c1b2f6ce6f7e8ad514c926e
obbteam <73595348+obbteam@users.noreply.github.com>
2024-04-19 14:35:17 +0200

    Time: 0 ms (100.00%) | Memory: 10.4 MB (87.09%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int prevSum = 0;
+        for(int i=0; i<nums.size();i++){
+            nums[i] += prevSum;
+            prevSum = nums[i];
+        }
+        return nums;
+    }
+};
