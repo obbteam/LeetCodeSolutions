a5bd1765ef8c0bbe94cbcb3ef135580ff2fb72b7
obbteam <73595348+obbteam@users.noreply.github.com>
2025-05-08 02:57:43 +0200

    Time: 0 ms (100.00%) | Memory: 26.1 MB (60.80%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,50 +1,28 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        // skip while height = 0
-        size_t first = 0;
-        while(height[first]==0 && first != height.size() - 1) {
-            ++first;
-        }
-        
-        if(first == height.size() - 1) return 0;
-        int area = 0;
+    int trap(std::vector<int>& height) {
+    int area = 0;
+    int* l = &height.front();
+    int* r = &height.back();
+    int leftMax = *l;
+    int rightMax = *r;
 
-
-        while(first != height.size() - 1) {
-
-            // skip to find the left wall
-            while(height[first + 1]> height[first] && first + 1 != height.size() - 1) {
-                ++first;
-            }
-
-            // skip to find the right wall
-            auto second = first + 1;
-            
-            while(height[second] < height[first] && second != height.size() - 1) {
-                ++second;
-            }
-
-            //check the if there is
-            if(second == height.size() - 1) {
-                    auto max = second;
-                    while(first < second) {
-                        if(height[second] >= height[max]) max = second;
-                        second--;
-                    }
-                    second = max;
-            }
-
-            // if we are here we found the right wall and place for rain to collect
-            area += std::min(height[first],height[second]) * (second - first - 1);
-            ++first;
-            while(first < second) {
-                area -= height[first++];
-            }
+    while (l < r) {
+        if (*l <= rightMax) {
+            l++;
+            if (leftMax - *l > 0)
+                area += leftMax - *l;
+            leftMax = std::max(*l, leftMax);
         }
 
-        
-
-        return area;
+        if (*r < leftMax) {
+            r--;
+            if (rightMax - *r > 0)
+                area += rightMax - *r;
+            rightMax = std::max(*r, rightMax);
+        }
     }
+
+    return area;
+}
 };
