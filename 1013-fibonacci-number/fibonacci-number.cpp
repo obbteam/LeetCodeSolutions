b905f24b1cbdd46f2831c443cb487312958cd226
obbteam <73595348+obbteam@users.noreply.github.com>
2025-05-21 00:03:40 +0200

    Time: 2 ms (76.03%) | Memory: 7.8 MB (37.58%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        int Fibonacci[31] = {0,1};
-        for(int k = 2; k<=n; k++) {
-            Fibonacci[k] = Fibonacci[k-1] + Fibonacci[k-2];
+        if (n <= 1) return n;
+        int prev2 = 0;
+        int prev1 = 1;
+        int res = 0;
+        for (int i = 2; i <= n; i++) {
+            res = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = res;
         }
-        return Fibonacci[n];
+        return res;
     }
 };
