08dbdcff6a8e7957d27e3eb7b33013d8c6bae996
obbteam <73595348+obbteam@users.noreply.github.com>
2025-07-30 17:53:08 +0200

    Time: 1 ms (50.58%) | Memory: 115.7 MB (23.30%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     bool increasingTriplet(std::vector<int>& nums) {
-        if (nums.size() < 3)
-            return false;
-        std::unordered_set<int> uniqueNums;
-        for (int j = 1; j < nums.size() - 1; j++) {
-            int i = j - 1, k = j + 1;
-            if (uniqueNums.contains(nums[i]) && uniqueNums.contains(nums[j]) && uniqueNums.contains(nums[k]))
-                continue;
+        int min1 = INT_MAX;
+        int min2 = INT_MAX;
 
-            uniqueNums.insert(nums[j]);
-
-            while (i >= 0 && k < nums.size()) {
-                if (nums[i] < nums[j] && nums[j] < nums[k])
-                    return true;
-                if (nums[i] >= nums[j])
-                    i--;
-                if (nums[k] <= nums[j])
-                    k++;
-            }
+        for (auto& n : nums) {
+            if (n <= min1)
+                min1 = n;
+            else if (n <= min2)
+                min2 = n;
+            else
+                return true;
         }
-
         return false;
     }
 };
