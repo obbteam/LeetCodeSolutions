8e1eec9efe2e515694ab936f8f4bf9f00a0b9556
obbteam <73595348+obbteam@users.noreply.github.com>
2025-08-11 20:36:10 +0500

    Time: 338 ms (5.32%) | Memory: 194 MB (5.85%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    
-std::vector<int> productQueries(int n, std::vector<std::vector<int> > queries) {
-    std::vector<int> powers, result;
-    auto x = std::bitset<64>(n);
-    for (int i = 0; i < 64; i++) {
-        if (x[i] == 1)
-            powers.push_back(std::pow(2, i));
-    }
-    int modulo = std::pow(10, 9) + 7;
-
-    for (auto q: queries) {
-        auto start = q[0];
-        auto end = q[1];
-        long long mul = 1;
-        for (int i = start; i <= end; i++) {
-                        mul = (mul * powers[i]) % modulo;
+    std::vector<int> productQueries(int n,
+                                    std::vector<std::vector<int>> queries) {
+        std::vector<int> exponents, result;
+        int i = 0, j = 0;
+        while (n) {
+            if (n & 1) {
+                exponents.push_back(i);
+                if (j)
+                    exponents[j] += exponents[j - 1];
+                j++;
+            }
+            n = n >> 1;
+            i++;
         }
-        result.push_back(mul);
+        int modulo = std::pow(10,9) + 7;
+        for (auto q : queries) {
+            int p = exponents[q[1]] - (q[0] ? exponents[q[0] - 1] : 0);
+            int res = 1;
+            for (int i = 0; i < p; i++) {
+                res = (res * 2) % modulo;
+            }
+            result.push_back(res);
+        }
+
+        return result;
     }
-
-
-    return result;
-}
 };
