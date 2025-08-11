35a17283962458591ad968be1ecaf28e372b1871
obbteam <73595348+obbteam@users.noreply.github.com>
2025-08-11 21:39:17 +0500

    Time: 80 ms (32.45%) | Memory: 173.5 MB (5.85%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    std::vector<int> productQueries(int n,
-                                    std::vector<std::vector<int>> queries) {
-        std::vector<int> exponents, result;
-        int i = 0, j = 0;
-        while (n) {
-            if (n & 1) {
-                exponents.push_back(i);
-                if (j)
-                    exponents[j] += exponents[j - 1];
-                j++;
-            }
-            n = n >> 1;
-            i++;
-        }
-        for (auto q : queries) {
-            int p = exponents[q[1]] - (q[0] ? exponents[q[0] - 1] : 0);
-            int res = 1;
-            result.push_back(modPow(2,p));
-        }
+   std::vector<int> productQueries(int n,
+                                std::vector<std::vector<int> > queries) {
+    int power = 1, MOD = (int) (1e9 + 7);
+    while (power <= n) power <<= 1;
+    power >>= 1;
 
-        return result;
+    std::vector<int> powers;
+    while (n > 0) {
+        if (power <= n) {
+            powers.push_back(power);
+            n -= power;
+        }
+        power >>= 1;
     }
 
-    long long modPow(long long base, long long exp) {
-        int MOD = 1000000007;
-        long long ans = 1;
-        while (exp > 0) {
-            if (exp & 1) ans = (ans * base) % MOD;
-            base = (base * base) % MOD;
-            exp >>= 1;
-        }
-        return ans;
+    n = powers.size();
+    std::vector<std::vector<int> > prefix(n, std::vector<int>(n, 0));
+    for (int i = 0; i < n; i++) {
+        prefix[i][i] = powers[n - 1 - i];
+        for (int j = i + 1; j < n; j++)
+            prefix[i][j] = static_cast<int>((1L * prefix[i][j - 1] * powers[n - 1 - j]) % MOD);
     }
+
+    std::vector<int> result(queries.size());
+    for (int i = 0; i < queries.size(); i++) {
+        result[i] = prefix[queries[i][0]][queries[i][1]];
+    }
+    return result;
+}
+
 
 };
