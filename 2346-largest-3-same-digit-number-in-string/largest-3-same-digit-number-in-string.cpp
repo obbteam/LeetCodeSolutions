d2a7c927693b462d65ad110dc153be4faa3f48ff
obbteam <73595348+obbteam@users.noreply.github.com>
2025-08-14 15:26:25 +0500

    Time: 3 ms (12.19%) | Memory: 9.1 MB (52.45%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     std::string largestGoodInteger(std::string num) {
-        if (num.length() < 3)
-            return "";
+    if (num.length() < 3)
+        return "";
 
-        std::string largestGoodInteger;
+    int uniqueDigit = -1;
 
-        for (int i = 2; i < num.length(); i++) {
-            if (num[i] == num[i - 1] && num[i - 1] == num[i - 2]) {
-                if (largestGoodInteger.empty() ||
-                    std::stoi(num.substr(i - 2, 3)) > std::stoi(largestGoodInteger))
-                    largestGoodInteger = num.substr(i - 2, 3);
-            }
+    for (int i = 2; i < num.length(); i++) {
+        if (num[i] == num[i - 1] && num[i - 1] == num[i - 2]) {
+            uniqueDigit = std::max(uniqueDigit, num[i] - '0');
         }
-
-        return largestGoodInteger;
+        if (uniqueDigit == 9)
+            break;
     }
+
+    if (uniqueDigit == -1)
+        return "";
+
+    return std::string(3, '0' + uniqueDigit);
+}
+
 };
