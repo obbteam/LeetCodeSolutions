8a6238d1519c68a36461841a02619954423bff2f
obbteam <73595348+obbteam@users.noreply.github.com>
2025-08-24 21:52:07 +0500

    Time: 0 ms (100.00%) | Memory: 59.9 MB (85.53%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int longestSubarray(std::vector<int>& nums) {
-        int numZeros = 0, maxOnes = 0;
-
-        auto start = 0, end = 0;
+        int start = 0, end = 0, maxOnes = 0, numZeros = 0;
 
         while (end < nums.size()) {
-            if (nums[end] == 1) {
+            if (nums[end] == 1)
                 end++;
-            } else if (nums[end] == 0 && numZeros == 0) {
+            else if (nums[end] == 0 && numZeros == 0) {
+                end++;
                 numZeros++;
-                end++;
             } else {
                 numZeros -= nums[start] == 0 ? 1 : 0;
                 start++;
             }
-
-            maxOnes = std::max(maxOnes, end - start);
+            maxOnes = max(maxOnes, end - start - 1);
         }
-
-        return maxOnes - 1;
+        return maxOnes;
     }
 };
