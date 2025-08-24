05673957e0cb8fa436430dd79c26b01430e2a2ee
obbteam <73595348+obbteam@users.noreply.github.com>
2025-08-25 00:28:10 +0500

    Time: 0 ms (100.00%) | Memory: 18.4 MB (85.89%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -6,21 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    bool isMirror(TreeNode* root1, TreeNode* root2) {
-        if(root1 == nullptr && root2 == nullptr) return true;
-        if(root1 == nullptr || root2 == nullptr) return false;
+    bool isMirrored(TreeNode* root1, TreeNode* root2) {
+        if (root1 == nullptr && root2 == nullptr)
+            return true;
+        if (root1 == nullptr || root2 == nullptr)
+            return false;
 
-        if(root1->val == root2->val && isMirror(root1->left, root2->right) && isMirror(root1->right, root2->left)) return true;
-        
+        if (root1->val == root2->val &&
+            isMirrored(root1->left, root2->right) &&
+            isMirrored(root1->right, root2->left))
+            return true;
+
         return false;
     }
 
-    bool isSymmetric(TreeNode* root) {
-        return isMirror(root, root);
-    }
+    bool isSymmetric(TreeNode* root) { return isMirrored(root, root); }
 };
