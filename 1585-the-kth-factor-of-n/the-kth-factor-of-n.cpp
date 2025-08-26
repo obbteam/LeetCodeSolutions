c4d93e735bdaaf98bb9c3fe760a0c510d1ab4670
obbteam <73595348+obbteam@users.noreply.github.com>
2025-08-26 19:24:38 +0500

    Time: 0 ms (100.00%) | Memory: 8.1 MB (53.87%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        std::vector<int> factors;
+        int count = 0;
         for (int i = 1; i <= n; i++) {
             if (n % i == 0)
-                factors.push_back(i);
-            if (factors.size() == k)
+                count++;
+            if (count == k)
                 return i;
         }
         return -1;
