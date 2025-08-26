e71f865b33c4a6c75d663e844eb3632eddc52fdd
obbteam <73595348+obbteam@users.noreply.github.com>
2025-08-26 19:57:04 +0500

    Time: 11 ms (66.23%) | Memory: 13 MB (91.19%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int partitionString(string s) {
         vector<char> arr(26, 0);
+        vector<char> cleaned(26, 0);
         int count = 1;
-        for (auto &c : s) {
-            if (arr[c - 'a'] == 1){
+        for (auto& c : s) {
+            if (arr[c - 'a'] == 1) {
                 count++;
-                arr = vector<char>(26,0);
+                arr = cleaned;
             }
-            arr[c-'a']++;
+            arr[c - 'a']++;
         }
 
         return count;
