88704936656a33c366ed1d3e4f64f792b556e533
obbteam <73595348+obbteam@users.noreply.github.com>
2025-08-30 15:07:55 +0200

    Time: 10 ms (18.64%) | Memory: 25.8 MB (12.36%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,46 +1,22 @@
 class Solution {
 public:
-    vector<int> validRowColBox = {0, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    bool isValidSudoku(vector<vector<char> > &board) {
+    unordered_set<char> row[9], cols[9], boxes[9];
 
-    bool isValidSudoku(vector<vector<char>>& board) {
-        for (const auto& row : board) {
-            auto valid = validRowColBox;
-            for (const auto el : row) {
-                if (el != '.') {
-                    valid[el - '0']--;
-                    if (valid[el - '0'] < 0)
-                        return false;
-                }
-            }
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[i].size(); j++) {
+            if (board[i][j] == '.')
+                continue;
+            const auto &c = board[i][j];
+            auto boxIdx = (i / 3) * 3 + (j / 3);
+            if (row[i].contains(c) || cols[j].contains(c) || boxes[boxIdx].contains(c))
+                return false;
+            row[i].insert(c);
+            cols[j].insert(c);
+            boxes[boxIdx].insert(c);
         }
-        for (int i = 0; i < board[0].size(); i++) {
-            auto valid = validRowColBox;
-            for (auto& el : board) {
-                if (el[i] != '.') {
-                    valid[el[i] - '0']--;
-                    if (valid[el[i] - '0'] < 0)
-                        return false;
-                }
-            }
-        }
-
-        // 9 squares to check
-        for (int xOffset = 0; xOffset < 9; xOffset += 3) {
-            for (int yOffset = 0; yOffset < 9; yOffset += 3) {
-                auto valid = validRowColBox;
-
-                for (int i = 0 + xOffset; i < 3 + xOffset; i++) {
-                    for (int j = 0 + yOffset; j < 3 + yOffset; j++) {
-                        if (board[i][j] != '.') {
-                            valid[board[i][j] - '0']--;
-                            if (valid[board[i][j] - '0'] < 0)
-                                return false;
-                        }
-                    }
-                }
-            }
-        }
-
-        return true;
     }
+
+    return true;
+}
 };
