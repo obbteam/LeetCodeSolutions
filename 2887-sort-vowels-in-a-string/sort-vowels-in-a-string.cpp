959c00a969188f67d1b06641c3cc486fca45b72f
obbteam <73595348+obbteam@users.noreply.github.com>
2025-09-11 21:08:48 +0200

    Time: 27 ms (13.45%) | Memory: 17.1 MB (25.20%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-    std::string sortVowels(std::string s) {
-        std::vector<char> vowels;
+    bool isVowel(const char& c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
 
-        for (char c : s)
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
-                vowels.push_back(c);
+    std::string sortVowels(std::string s) {
+        std::unordered_map<char, int> vowels;
+
+        for (const char& c : s)
+            if (isVowel(c))
+                vowels[c]++;
 
         if (vowels.empty())
             return s;
 
-        std::sort(vowels.begin(), vowels.end());
-
         int j = 0;
-        for (char& i : s) {
-            if (i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u' ||
-                i == 'A' || i == 'E' || i == 'I' || i == 'O' || i == 'U')
-                i = vowels[j++];
-        }
-        return s;
+        std::string sortedVowels = "AEIOUaeiou";
+        std::string result;
+        for (const char& c : s)
+            if (!isVowel(c))
+                result += c;
+            else {
+                while (vowels[sortedVowels[j]] == 0)
+                    j++;
+                result += sortedVowels[j];
+                vowels[sortedVowels[j]]--;
+            }
+
+        return result;
     }
 };
