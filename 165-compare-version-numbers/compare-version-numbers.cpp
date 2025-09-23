426863ad6ce00469202f3f80ba5752a8b687b382
obbteam <73595348+obbteam@users.noreply.github.com>
2025-09-23 12:13:37 +0200

    Time: 0 ms (100.00%) | Memory: 8.4 MB (48.40%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        auto dot1 = version1.find_first_of('.');
-        auto dot2 = version2.find_first_of('.');
-        int toCompare1 = 0;
-        int toCompare2 = 0;
-        while (dot1 != string::npos || dot2 != string::npos) {
-            toCompare1 = dot1 == string::npos ? stoi(version1)
-                                              : stoi(version1.substr(0, dot1));
-            toCompare2 = dot2 == string::npos ? stoi(version2)
-                                              : stoi(version2.substr(0, dot2));
-            if (toCompare1 == toCompare2) {
-                version1 = dot1 == string::npos ? version1 = "0"
-                                                : version1.substr(dot1 + 1);
-                version2 = dot2 == string::npos ? version2 = "0"
-                                                : version2.substr(dot2 + 1);
-                dot1 = version1.find_first_of('.');
-                dot2 = version2.find_first_of('.');
-            } else if (toCompare1 > toCompare2)
-                return 1;
-            else
+        auto i1 = 0, i2 = 0;
+
+        while (i1 < version1.length() || i2 < version2.length()) {
+            std::string cur1, cur2;
+
+            while (i1 < version1.length() && version1[i1] != '.') {
+                cur1 += version1[i1];
+                ++i1;
+            }
+
+            while (i2 < version2.length() && version2[i2] != '.') {
+                cur2 += version2[i2];
+                ++i2;
+            }
+
+            int toCheck1 = cur1.empty() ? 0 : stoi(cur1);
+            int toCheck2 = cur2.empty() ? 0 : stoi(cur2);
+
+            if (toCheck1 < toCheck2)
                 return -1;
+            if (toCheck1 > toCheck2)
+                return 1;
+
+            ++i1;
+            ++i2;
         }
-        toCompare1 = stoi(version1);
-        toCompare2 = stoi(version2);
-        if (toCompare1 > toCompare2)
-            return 1;
-        else if (toCompare1 < toCompare2)
-            return -1;
-        else
-            return 0;
+        return 0;
     }
 };
