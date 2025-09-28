b96f0eed0979ed8db68705852d1a931ea603c679
obbteam <73595348+obbteam@users.noreply.github.com>
2025-09-28 21:00:20 +0200

    Time: 0 ms (100.00%) | Memory: 25.7 MB (46.12%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -4,12 +4,8 @@ public:
         std::sort(nums.begin(), nums.end());
 
         for (int i = nums.size() - 1; i >= 2; --i) {
-            for (int j = i - 1; j >= 1; --j) {
-                for (int k = j - 1; k >= 0; --k) {
-                    if (nums[i] < nums[j] + nums[k])
-                        return nums[i] + nums[j] + nums[k];
-                }
-            }
+            if (nums[i] < nums[i - 1] + nums[i - 2])
+                return nums[i] + nums[i-1] + nums[i-2];
         }
         return 0;
     }
