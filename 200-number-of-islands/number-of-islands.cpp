9bea76f03b634bf91e17052e050d918be3dbe402
obbteam <73595348+obbteam@users.noreply.github.com>
2025-10-21 12:28:55 +0200

    Time: 123 ms (5.04%) | Memory: 34.3 MB (5.04%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
-    vector<pair<int,int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-
-    void bfs(int i, int j, vector<vector<char>>& grid, unordered_set<string>& visited) {
-        queue<vector<int>> q;
-        visited.insert(to_string(i) + ',' + to_string(j));
-
-        q.push({i, j});
-        while (!q.empty()) {
-            auto v = q.front(); q.pop();
-            int x = v[0], y = v[1];
-            
-            for (auto [dx,dy] : directions) {
-                auto nx = x + dx;
-                auto ny = y + dy;
-                if (nx >= 0 && nx < grid.size() && ny >= 0 && ny < grid[0].size() && grid[nx][ny] == '1' && !visited.contains(to_string(nx) + ',' + to_string(ny))) {
-                    q.push({nx,ny});
-                    visited.insert(to_string(nx) + ',' + to_string(ny));
-                }
-            }            
-        }
-    }
-
     int numIslands(vector<vector<char>>& grid) {
+        int islands = 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
         unordered_set<string> visited;
-        int num = 0;
-        for (int i = 0; i < grid.size(); ++i) {
-            for (int j = 0; j < grid[i].size(); ++j) {
-                if (grid[i][j] == '1' && !visited.contains(to_string(i) + ',' + to_string(j))) {
-                    bfs(i, j, grid, visited);
-                    num++;
+
+        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '1' && visited.find(to_string(r) + "," + to_string(c)) == visited.end()) {
+                    islands++;
+                    bfs(grid, r, c, visited, directions, rows, cols);
                 }
             }
         }
 
-        return num;
+        return islands;        
     }
-};
+
+private:
+    void bfs(vector<vector<char>>& grid, int r, int c, unordered_set<string>& visited, vector<pair<int, int>>& directions, int rows, int cols) {
+        queue<pair<int, int>> q;
+        visited.insert(to_string(r) + "," + to_string(c));
+        q.push({r, c});
+
+        while (!q.empty()) {
+            auto [row, col] = q.front();
+            q.pop();
+
+            for (auto [dr, dc] : directions) {
+                int nr = row + dr;
+                int nc = col + dc;
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '1' && visited.find(to_string(nr) + "," + to_string(nc)) == visited.end()) {
+                    q.push({nr, nc});
+                    visited.insert(to_string(nr) + "," + to_string(nc));
+                }
+            }
+        }
+    }
+};    
