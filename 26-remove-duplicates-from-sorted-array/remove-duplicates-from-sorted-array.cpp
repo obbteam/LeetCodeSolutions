f10cdfe55227ec0c7ff9132bc229420c579048c1
obbteam <73595348+obbteam@users.noreply.github.com>
2025-10-28 16:25:01 +0100

    Time: 0 ms (100.00%) | Memory: 22.7 MB (19.28%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty())
-            return 0; // If the vector is empty, return 0.
-
-        int k =
-            1; // Start counting from the first element, assuming it's unique.
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] != nums[i - 1]) { // Only if the current element is
-                                          // different from the previous
-                nums[k] = nums[i]; // Place it in the position of the next
-                                   // unique element.
-                k++;               // Increment the count of unique elements.
+        int k = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] != nums[i-1]) {
+                nums[k] = nums[i];
+                k++;
             }
         }
-        return k; // The array from index 0 to k-1 now contains unique elements.
+        return k;
     }
 };
