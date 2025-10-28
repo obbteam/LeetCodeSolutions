3cef3dd9ed4e86d87dca1891a404fdf0f3a99e92
obbteam <73595348+obbteam@users.noreply.github.com>
2025-10-28 14:07:04 +0100

    Time: 0 ms (100.00%) | Memory: 22 MB (67.89%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -1,45 +1,21 @@
 class Solution {
 public:
-    vector<int> dirs = {1, -1};
-
-    int helper(int i, int nonZeros, vector<int>& nums) {
-        int count = 0;
-
-        for (auto d : dirs) {
-            auto cp = nums;
-            int nz = nonZeros;
-            int cur = i;
-            while (nz > 0 && cur >= 0 && cur < nums.size()) {
-                if (cp[cur] > 0) {
-                    cp[cur]--;
-
-                    if (cp[cur] == 0)
-                        nz--;
-                    d *= -1;
-                }
-                cur += d;
-            }
-
-            if (nz == 0)
-                count++;
-        }
-
-        return count;
-    }
-
     int countValidSelections(vector<int>& nums) {
-        int count = 0, nonZeros = 0;
+        int leftSum = 0, ans = 0, rightSum = 0;
+        for (auto x : nums) rightSum+=x;
 
         for (auto x : nums) {
-            if (x > 0) {
-                nonZeros++;
+            if (x == 0) {
+                if (leftSum == rightSum || leftSum - rightSum == 1)
+                    ans++;
+                if (leftSum == rightSum || rightSum - leftSum == 1)
+                    ans++;
+            } else {
+                leftSum += x;
+                rightSum -=x;
             }
         }
-
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == 0)
-                count += helper(i, nonZeros, nums);
-        }
-        return count;
+        
+        return ans;
     }
 };
