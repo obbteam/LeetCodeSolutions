a615ddb9395df5f61f5701ca6c8dd684b5f6b014
obbteam <73595348+obbteam@users.noreply.github.com>
2025-10-28 17:23:59 +0100

    Time: 0 ms (100.00%) | Memory: 30.5 MB (17.28%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> rotated = nums;
+        for (int i = 0; i < nums.size(); ++i) {
+            rotated[(i + k) % nums.size()] = nums[i];
+        }
+        nums = rotated;
+    }
+};
