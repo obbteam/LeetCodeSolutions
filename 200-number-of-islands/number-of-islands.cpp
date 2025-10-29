489598d107d6d75279ff020df85f71e85c4f7bbf
obbteam <73595348+obbteam@users.noreply.github.com>
2025-10-29 16:16:19 +0100

    Time: 25 ms (72.22%) | Memory: 23.8 MB (24.77%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
-
-    void dfs(vector<vector<char>>& grid , vector<vector<int>>& vis , int i , int j , int n , int m){
-        if(i < 0 || i > n-1 || j < 0 || j > m-1 || vis[i][j] == 1 || grid[i][j] == '0') return;
-        vis[i][j] = 1;
-        dfs(grid , vis , i+1 , j , n , m);
-        dfs(grid , vis , i , j+1 , n , m);
-        dfs(grid , vis , i , j-1 , n , m);
-        dfs(grid , vis , i-1 , j , n , m);
-        return;
-    }
+    vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> vis(n , vector<int>(m , 0));
-        int cnt = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == '1' && vis[i][j] == 0) {
-                    dfs(grid , vis , i , j , n , m);
-                    cnt++;
+        int islands = 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
+        vector<vector<bool>> visited(rows, vector<bool>(cols, 0));
+
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '1' && visited[r][c] == 0) {
+                    islands++;
+                    bfs(grid, r, c, visited, rows, cols);
                 }
             }
         }
-        return cnt;
+
+        return islands;        
     }
-};
+
+private:
+    void bfs(vector<vector<char>>& grid, int r, int c, vector<vector<bool>>& visited, int rows, int cols) {
+        queue<pair<int, int>> q;
+        visited[r][c] = 1;
+        q.push({r, c});
+
+        while (!q.empty()) {
+            auto [row, col] = q.front();
+            q.pop();
+
+            for (auto [dr, dc] : directions) {
+                int nr = row + dr;
+                int nc = col + dc;
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '1' && visited[nr][nc] == 0) {
+                    q.push({nr, nc});
+                    visited[nr][nc] = 1;
+                }
+            }
+        }
+    }
+};    
