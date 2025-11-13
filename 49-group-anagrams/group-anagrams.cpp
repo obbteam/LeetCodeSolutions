3f08adb2737223755fdb21242b3edccf5cadf1c1
obbteam <73595348+obbteam@users.noreply.github.com>
2025-11-13 18:50:02 +0100

    Time: 18 ms (52.57%) | Memory: 26.2 MB (32.17%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> map;
+            
+        for (auto s : strs) {
+            auto key = s;
+            sort(key.begin(), key.end());
+            map[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+        for(auto [key, words] : map) {
+            ans.push_back(words);
+        }
+        return ans;
+    }
+};
