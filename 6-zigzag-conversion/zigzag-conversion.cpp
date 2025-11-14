60edf36b1a864fa370096dca0c5d50c4a378de91
obbteam <73595348+obbteam@users.noreply.github.com>
2025-11-15 00:41:36 +0100

    Time: 4 ms (64.02%) | Memory: 13.9 MB (65.31%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -4,7 +4,7 @@ public:
         if (numRows == 1) return s;
         vector<string> conv(numRows, "");
         int i = 0, direction = 1;
-        for(auto c : s) {
+        for(auto &c : s) {
             if (i == 0) direction = 1;
             else if (i == numRows - 1) direction = -1;
             conv[i] += c;
@@ -13,7 +13,7 @@ public:
 
         string converted = "";
         converted.reserve(s.length());
-        for(auto s : conv) {
+        for(auto &s : conv) {
             converted += s;
         }
         return converted;
