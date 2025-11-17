c64a50941cab9525955dbaf1022430023c821d2e
obbteam <73595348+obbteam@users.noreply.github.com>
2025-11-17 18:31:22 +0100

    Time: 0 ms (100.00%) | Memory: 71.9 MB (5.86%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0, ans = INT_MIN;
+        for (auto x : nums) {
+            sum += x;
+            ans = max(sum, ans);
+            if (sum < 0) sum = 0;
+        }
+        return ans;
+    }
+};
