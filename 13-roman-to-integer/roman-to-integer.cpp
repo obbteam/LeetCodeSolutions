91fc32c15a1f3a7d1072b35af8696444c41da6a1
obbteam <73595348+obbteam@users.noreply.github.com>
2026-02-12 13:52:54 +0100

    Time: 13 ms (13.78%) | Memory: 12.3 MB (62.05%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,49 +1,24 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int i = 0;
-        int result = 0;
-        while (s[i] != '\0') {
-            switch (s[i]) {
-            case 'I':
-                if (s[i + 1] == 'V' || s[i + 1] == 'X') {
-                    result--;
-                } else
-                    result++;
-                i++;
-                break;
-            case 'V':
-                result += 5;
-                i++;
-                break;
-            case 'X':
-                if (s[i + 1] == 'L' || s[i + 1] == 'C') {
-                    result -= 10;
-                } else
-                    result += 10;
-                i++;
-                break;
-            case 'L':
-                result += 50;
-                i++;
-                break;
-            case 'C':
-                if (s[i + 1] == 'D' || s[i + 1] == 'M') {
-                    result -= 100;
-                } else
-                    result += 100;
-                i++;
-                break;
-            case 'D':
-                result += 500;
-                i++;
-                break;
-            case 'M':
-                result += 1000;
-                i++;
-                break;
-            }
+        if (s.length() == 1)
+            return romanChars[s[0]];
+
+        int res = 0;
+
+        for (int i = 0; i < s.length() - 1; ++i) {
+            auto cur = romanChars[s[i]];
+            auto next = romanChars[s[i+1]];
+
+            if (cur < next) res -= cur;
+            else res += cur;
         }
-        return result;
+        res+=romanChars[s[s.length() - 1]];
+        return res;
     }
+
+private:
+    std::unordered_map<char, int> romanChars = {
+        {'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
+        {'C', 100}, {'D', 500}, {'M', 1000}};
 };
